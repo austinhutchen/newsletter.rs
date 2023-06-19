02b9d40a5da79630c8332d666bbeb58ebd035251
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-06-19 07:41:03 -0700

    port fix

--- src/main.rs
+++ src/main.rs
@@ -5,8 +5,9 @@ use std::net::TcpListener;
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    let address: &str = "http://127.0.0.1:";
+    let address: &str = "127.0.0.1:8000";
+    // was listen bind successful for random TCP choice of local port?
     let listener:TcpListener = TcpListener::bind(address).expect("Failed to call TCP LISTEN");
-    // is run(addr) an error?  await if so. else, return the server result
+    // is run(listener) an error?  await if so. else, return the server result
     run(listener)?.await
 }
