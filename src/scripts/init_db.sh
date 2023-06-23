0e7e865ebcc150e90d93ded042c65579d0acf301
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-06-23 09:17:17 -0700

    3.8.4.2.3

--- src/scripts/init_db.sh
+++ src/scripts/init_db.sh
@@ -1,6 +1,18 @@
 #!/usr/bin/env bash
 set -x
 set -eo pipefail
+# ensure that psql and swlx are installed for proper script execution of server and init database
+if ! [ -x "$(command -v psql)" ]; then
+    echo >&2 "Error: psql is not installed."
+    exit 1
+fi
+if ! [ -x "$(command -v sqlx)" ]; then
+    echo >&2 "Error: sqlx is not installed."
+    echo >&2 "Use:"
+    echo >&2 "    cargo install --version=0.5.7 sqlx-cli --no-default-features --features postgres"
+    echo >&2 "to install it."
+    exit 1
+fi
 # Check if a custom user has been set, otherwise default to 'postgres'
 DB_USER=${POSTGRES_USER:=postgres}
 # Check if a custom password has been set, otherwise default to 'password'
@@ -11,10 +23,18 @@ DB_NAME="${POSTGRES_DB:=newsletter}"
 DB_PORT="${POSTGRES_PORT:=5432}"
 # Launch postgres using Docker
 docker run \
-  -e POSTGRES_USER=${DB_USER} \
-  -e POSTGRES_PASSWORD=${DB_PASSWORD} \
-  -e POSTGRES_DB=${DB_NAME} \
-  -p "${DB_PORT}":5432 \
-  -d postgres \
-  postgres -N 1000
-  # ^ Increased maximum number of connections for testing purposes
+-e POSTGRES_USER=${DB_USER} \
+-e POSTGRES_PASSWORD=${DB_PASSWORD} \
+-e POSTGRES_DB=${DB_NAME} \
+-p "${DB_PORT}":5432 \
+-d postgres \
+postgres -N 1000
+# ^ Increased maximum number of connections for testing purposes
+# Keep pinging Postgres until it's ready to accept commands
+export PGPASSWORD="${DB_PASSWORD}"
+until psql -h "localhost" -U "${DB_USER}" -p "${DB_PORT}" -d "postgres" -c '\q'; do
+    >&2 echo "Postgres is still unavailable - sleeping"
+sleep 1 done
+>&2 echo "Postgres is up and running on port ${DB_PORT}!"
+export DATABASE_URL=postgres://${DB_PASSWORD}@localhost:${DB_PORT}/${DB_NAME}
+sqlx database create
