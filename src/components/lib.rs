ad29179f97b0b335d1fe69ebfad8fb66ed94f6ed
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-06-23 09:07:46 -0700

    docker init

--- src/components/lib.rs
+++ src/components/lib.rs
@@ -5,7 +5,7 @@ use actix_web::{
     App, HttpRequest, HttpResponse, HttpServer, Responder,
 };
 use std::net::TcpListener;
-// declares struct for usage as form data
+// declares struct for usage as form data, allowing fromrequest implementation and data parsing from service body
 #[derive(serde::Deserialize)]
 struct FormData {
     email: String,
