c8942128ed11d2f034ad70174c0e8d25aa88eb17
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-06-24 16:09:25 -0700

    d

--- src/configuration.rs
+++ src/configuration.rs
@@ -1,31 +1,23 @@
-
-
-
-
-
-
-
-
-
-#[derive(serde::Deserialize)] pub struct Settings {
- pub database: DatabaseSettings,
- pub application_port: u16
+#[derive(serde::Deserialize)]
+pub struct Settings {
+    pub database: DatabaseSettings,
+    pub application_port: u16,
 }
-#[derive(serde::Deserialize)] pub struct DatabaseSettings { pub username: String, pub password: String,
- pub port: u16,
- pub host: String,
- pub database_name: String,
+#[derive(serde::Deserialize)]
+pub struct DatabaseSettings {
+    pub username: String,
+    pub password: String,
+    pub port: u16,
+    pub host: String,
+    pub database_name: String,
 }
 
-
-
-
-
-pub fn get_configuration() -> Result<Settings, config::ConfigError> { // Initialise our configuration reader
- let mut settings = config::Config::default();
- // Add configuration values from a file named `configuration`. // It will look for any top-level file with an extension
- // that `config` knows how to parse: yaml, json, etc. settings.merge(config::File::with_name("configuration"))?;
-     // Try to convert the configuration values it read into
-     // our Settings type
-     settings.try_into()
- }
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+    // Initialise our configuration reader
+    let mut settings = config::Config::default();
+    // Add configuration values from a file named `configuration`. // It will look for any top-level file with an extension
+    // that `config` knows how to parse: yaml, json, etc. settings.merge(config::File::with_name("configuration"))?;
+    // Try to convert the configuration values it read into Settings type and return Result wrapper
+    settings.merge(config::File::with_name("configuration"))?;
+    settings.try_into()
+}
--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use newsletter::run;
 use std::net::TcpListener;
 mod configuration;
 mod routes;
-pub use configuration::*;
+pub use configuration::get_configuration;
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     // Bubble up the io::Error if we failed to bind the address
     // Otherwise call .await on our Server
-    let address: &str = "127.0.0.1:8000";
+    let configuration = get_configuration().expect("Failed to read App configuration.");
+    let address= format!("127.0.0.1:{}",configuration.application_port);
     // was listen bind successful for random TCP choice of local port?
     let listener: TcpListener = TcpListener::bind(address).expect("Failed to call TCP LISTEN");
     // is run(listener) an error?  await if so. else, return the server result
