2e778c10a15d31726e65851092e6fc5f2ff3a18b
austinhutchen <93489691+austinhutchen@users.noreply.github.com>
2023-06-25 21:48:33 -0700

    done

--- src/routes/health_check.rs
+++ src/routes/health_check.rs
@@ -2,7 +2,7 @@ use std::net::TcpListener;
 
 // ! test.health_check.rs
 use crate::configuration::get_configuration;
-use sqlx::{PgConnection, Connection};
+use sqlx::{query, Connection, PgConnection};
 
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
@@ -29,24 +29,33 @@ async fn health_check_works() {
 
 #[tokio::test]
 async fn validsubscribe200() {
-
     let app_address = spawn_app();
-    let configuration = get_configuration().expect("Failed to read configuration"); let connection_string = configuration.database.connection_string();
+    let configuration = get_configuration().expect("Failed to read configuration");
+    let connection_string = configuration.database.connection_string();
     // The `Connection` trait MUST be in scope for us to invoke
     // `PgConnection::connect` - it is not an inherent method of the struct!
-    let connection = PgConnection::connect(&connection_string)
-    .await
-    .expect("Failed to connect to Postgres."); let client = reqwest::Client::new();
+    let mut connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+    let client = reqwest::Client::new();
     // Act
-        let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
-        let response = client
-    .post(&format!("{}/subscriptions", &app_address)) .header("Content-Type", "application/x-www-form-urlencoded") .body(body)
-    .send()
-            .await
-            .expect("Failed to execute request.");
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client
+        .post(&format!("{}/subscriptions", &app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await
+        .expect("Failed to execute request.");
     // Assert
-    assert_eq!(200, response.status().as_u16()); }
-
+    assert_eq!(200, response.status().as_u16());
+    let saved = query!("SELECT email, name FROM subscriptions")
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
+}
 
 #[tokio::test]
 async fn invalidform400() {
